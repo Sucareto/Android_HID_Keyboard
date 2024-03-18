0051f2c213bdb0a3d54be95f89176f5452a0c9a6
Sucareto <28331534+Sucareto@users.noreply.github.com>
2024-03-19 05:30:56 +0800

    调整滚轮区域，重写鼠标指针计算方式

--- app/src/main/java/org/sucareto/androidhidkeyboard/Mouse.java
+++ app/src/main/java/org/sucareto/androidhidkeyboard/Mouse.java
@@ -1,6 +1,5 @@
 package org.sucareto.androidhidkeyboard;
 
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,28 +10,21 @@ import android.view.View;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.math.MathUtils;
 import androidx.core.view.GestureDetectorCompat;
 
 import org.sucareto.androidhidkeyboard.databinding.ActivityMouseBinding;
 
 public class Mouse extends AppCompatActivity {
     HidController hid = new HidController();
-    private int MoveSpeed = 2;
-    private float ScrollData = 0;
+    private int moveSpeed = 3;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View root_view = ActivityMouseBinding.inflate(getLayoutInflater()).getRoot();
-        root_view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         setContentView(root_view);
 
         findViewById(R.id.BtnBack).setOnClickListener(v -> {
@@ -67,7 +59,7 @@ public class Mouse extends AppCompatActivity {
         ((SeekBar) findViewById(R.id.sbMouseMoveSpeed)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                MoveSpeed = progress;
+                moveSpeed = progress;
             }
 
             @Override
@@ -99,14 +91,13 @@ public class Mouse extends AppCompatActivity {
         @Override
         public boolean onTouch(View v, MotionEvent e) {
             switch (e.getAction()) {
-                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_DOWN -> {
                     v.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_PRESS);
                     hid.kPress((byte) Integer.parseInt(v.getTag().toString(), 16));
-                    break;
-                case MotionEvent.ACTION_UP:
-                case MotionEvent.ACTION_CANCEL:
+                }
+                case MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
                     hid.kRelease((byte) Integer.parseInt(v.getTag().toString(), 16));
-                    break;
+                }
             }
             return false;
         }
@@ -117,14 +108,13 @@ public class Mouse extends AppCompatActivity {
         @Override
         public boolean onTouch(View v, MotionEvent e) {
             switch (e.getAction()) {
-                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_DOWN -> {
                     v.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_PRESS);
                     hid.mPress((byte) Integer.parseInt(v.getTag().toString(), 16));
-                    break;
-                case MotionEvent.ACTION_UP:
-                case MotionEvent.ACTION_CANCEL:
+                }
+                case MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
                     hid.mRelease((byte) Integer.parseInt(v.getTag().toString(), 16));
-                    break;
+                }
             }
             return false;
         }
@@ -132,14 +122,14 @@ public class Mouse extends AppCompatActivity {
 
     class TouchAreaListener extends GestureDetector.SimpleOnGestureListener {
         @Override
-        public boolean onSingleTapConfirmed(MotionEvent e) {
+        public boolean onSingleTapConfirmed(@NonNull MotionEvent e) {
             hid.mPress((byte) 1);
             hid.mRelease((byte) 1);
             return true;
         }
 
         @Override
-        public boolean onDoubleTap(MotionEvent e) {
+        public boolean onDoubleTap(@NonNull MotionEvent e) {
             hid.mPress((byte) 1);
             hid.mRelease((byte) 1);
             hid.mPress((byte) 1);
@@ -148,23 +138,41 @@ public class Mouse extends AppCompatActivity {
         }
 
         @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            hid.mCode[1] = (byte) MathUtils.clamp((-distanceX * MoveSpeed), -127, 127);
-            hid.mCode[2] = (byte) MathUtils.clamp((-distanceY * MoveSpeed), -127, 127);
-            hid.mSend();
+        public boolean onScroll(MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
+            int moveX = (int) (-distanceX * moveSpeed);
+            int moveY = (int) (-distanceY * moveSpeed);
+            while (moveX != 0 || moveY != 0) {
+                if (Math.abs(moveX) > 127) {
+                    hid.mCode[1] = (byte) (moveX > 0 ? 127 : -127);
+                    moveX -= hid.mCode[1];
+                } else {
+                    hid.mCode[1] = (byte) moveX;
+                    moveX = 0;
+                }
+                if (Math.abs(moveY) > 127) {
+                    hid.mCode[2] = (byte) (moveY > 0 ? 127 : -127);
+                    moveY -= hid.mCode[2];
+                } else {
+                    hid.mCode[2] = (byte) moveY;
+                    moveY = 0;
+                }
+                hid.mSend();
+            }
             return true;
         }
 
         @Override
-        public void onLongPress(MotionEvent e) {
+        public void onLongPress(@NonNull MotionEvent e) {
             hid.mPress((byte) 2);
             hid.mRelease((byte) 2);
         }
     }
 
     class ScrollAreaListener extends GestureDetector.SimpleOnGestureListener {
+        private float ScrollData = 0;
+
         @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        public boolean onScroll(MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
             ScrollData += distanceY;
             if (Math.abs(ScrollData) > 20) {
                 hid.mCode[3] = (byte) (ScrollData < 0 ? 1 : 255);
